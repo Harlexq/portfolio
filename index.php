18218e08782eeab14f36a6d1e3d75a838fbff489
Harlexq <serhanbakir5@gmail.com>
2023-08-14 13:39:20 +0300

    py

--- index.php
+++ index.php
@@ -14,13 +14,42 @@
 
 <body>
 
-    <?php
-    include('pages/common/header.php');
-    ?>
-
-    <?php
-    include('pages/common/footer.php');
-    ?>
+    <div class="inclusive">
+        <header class="header">
+            <div class="header-inner">
+                <div class="navbar-brand">
+                    <a href="index.php" class="logo">
+                        <img src="assets/img/SerhanBakir.jpg" alt="">
+                        <h1>Serhan Bakır</h1>
+                    </a>
+                </div>
+                <nav class="navbar">
+                    <a href="#home" class="nav-item active">Home</a>
+                    <a href="#about" class="nav-item">About Me</a>
+                    <a href="#whatıdo" class="nav-item">What I Do</a>
+                    <a href="#resume" class="nav-item">Resume</a>
+                    <a href="#portfolio" class="nav-item">Portfolio</a>
+                    <a href="#testimonial" class="nav-item">Testimonial</a>
+                    <a href="#contact" class="nav-item">Contact</a>
+                </nav>
+                <nav class="social">
+                    <a href="https://github.com/Harlexq" target="_blank" class="social-item">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    <span>Github</span>
+                    <a href="https://www.linkedin.com/in/serkan-bakir/" target="_blank" class="social-item">
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                    <a href="https://twitter.com/harlexq" target="_blank" class="social-item">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://twitter.com/harlexq" target="_blank" class="social-item">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                </nav>
+            </div>
+        </header>
+    </div>
 
     <script src="assets/js/main.js"></script>
 
