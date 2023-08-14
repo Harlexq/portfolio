62e14fee1b07e93e3b7cb49dde0fb2f4b0e62d1e
Harlexq <serhanbakir5@gmail.com>
2023-08-14 22:28:03 +0300

    harlex

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                 <nav class="navbar">
                     <a href="#home" class="nav-item active">Home</a>
                     <a href="#about" class="nav-item">About Me</a>
-                    <a href="#whatıdo" class="nav-item">What I Do</a>
+                    <a href="#services" class="nav-item">What I Do</a>
                     <a href="#resume" class="nav-item">Resume</a>
                     <a href="#portfolio" class="nav-item">Portfolio</a>
                     <a href="#testimonial" class="nav-item">Testimonial</a>
@@ -34,21 +34,126 @@
                 </nav>
                 <nav class="social">
                     <a href="https://github.com/Harlexq" target="_blank" class="social-item">
+                        <span class="social-tooltip">Github</span>
                         <i class="fa-brands fa-github"></i>
                     </a>
-                    <span>Github</span>
                     <a href="https://www.linkedin.com/in/serkan-bakir/" target="_blank" class="social-item">
+                        <span class="social-tooltip">Linkedin</span>
                         <i class="fa-brands fa-linkedin-in"></i>
                     </a>
                     <a href="https://twitter.com/harlexq" target="_blank" class="social-item">
+                        <span class="social-tooltip">Instagram</span>
                         <i class="fa-brands fa-instagram"></i>
                     </a>
                     <a href="https://twitter.com/harlexq" target="_blank" class="social-item">
+                        <span class="social-tooltip">Twitter</span>
                         <i class="fa-brands fa-twitter"></i>
                     </a>
                 </nav>
             </div>
         </header>
+        <div class="pages">
+            <section class="home" id="home">
+                <div class="home-inner">
+                    <div class="home-content">
+                        <h2>Welcome</h2>
+                        <div class="wrapper">
+                            <div class="static-text">I'm</div>
+                            <ul class="dynamic-text">
+                                <li>
+                                    <span>Serhan Bakır</span>
+                                </li>
+                                <li>
+                                    <span>Frontend Developer</span>
+                                </li>
+                                <li>
+                                    <span>Web Designer</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <a href="#" class="hireme">Hire Me</a>
+                    </div>
+                </div>
+                <a href="#about" class="icon-scroll">
+                    <svg viewBox="0 0 42.5 42.5">
+                        <path d="M21 3C14.4 3 9 8.4 9 15v10c0 6.6 5.4 13 12 13s12-6.4 12-13V15c0-6.6-5.4-12-12-12zm10 21.5C31 30.4 26.5 36 21 36s-10-5.6-10-11.5v-8.9C11 9.8 15.5 5 21 5s10 4.8 10 10.6v8.9z" id="Rounded_Rectangle_1_copy" class="outline"></path>
+                        <circle cx="21" cy="15" r="2" id="Ellipse_1" class="st0"></circle>
+                    </svg>
+                </a>
+            </section>
+            <section class="pages-content" id="about">
+                <div class="container">
+                    <div class="pages-inner">
+                        <div class="content-title">
+                            <h2>About Me</h2>
+                            <div class="content-text">
+                                <p>Know Me More</p>
+                                <span class="content-seperator"></span>
+                            </div>
+                        </div>
+                        <div class="about-content">
+                            <div class="about-text">
+                                <h2>I'm <span>Serhan Bakır,</span> a Frontend Developer</h2>
+                                <p>Hello, I'm Serhan Bakır. In order to enhance my skills in web development and make a solid start to my career, I completed an internship as a Junior Web Developer at an Advertising Agency. I possess intermediate-level knowledge of HTML, CSS, Bootstrap, and JavaScript, and I successfully apply these skills in my projects.<br><br>Furthermore, I continuously update myself on web design, user experience, and digital marketing trends.<br><br>Among my professional goals is designing creative, functional, and impactful websites. I strive to improve myself within the industry, collaborate with fellow experts, and provide a better web development experience by understanding the needs of clients.</p>
+                            </div>
+                            <div class="about-info">
+                                <ul class="about-info-content">
+                                    <li class="about-info-item">
+                                        <span>Name:</span>
+                                        <p>Serhan Bakır</p>
+                                    </li>
+                                    <li class="about-info-item">
+                                        <span>E-Mail:</span>
+                                        <a href="mailto:serhanbakir5@gmail.com" target="_blank">serhanbakir5@gmail.com</a>
+                                    </li>
+                                    <li class="about-info-item">
+                                        <span>Age:</span>
+                                        <p>17</p>
+                                    </li>
+                                    <li class="about-info-item">
+                                        <span>From:</span>
+                                        <p>Türkiye, Gaziantep</p>
+                                    </li>
+                                </ul>
+                                <div class="about-cv">
+                                    <a href="assets/img/cv.pdf" download="">Download CV</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="pages-content services" id="services">
+                <div class="container">
+                    <div class="pages-inner">
+                        <div class="content-title">
+                            <h2>Services</h2>
+                            <div class="content-text">
+                                <p>What I Do?</p>
+                                <span class="content-seperator"></span>
+                            </div>
+                        </div>
+                        <div class="services-content">
+                            <div class="services-item">
+                                <i class="fa-solid fa-desktop"></i>
+                                <div class="sevices-item-text">
+                                    <h3>Web Design</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, illo!</p>
+                                </div>
+                            </div>
+                            <div class="services-item">
+                                <i class="fa-solid fa-code"></i>
+                                <div class="sevices-item-text">
+                                    <h3>Frontend Developer</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, illo!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
 
     <script src="assets/js/main.js"></script>
