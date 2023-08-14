a806255da6b33126c9214f13a24b5632c6af3616
Harlexq <serhanbakir5@gmail.com>
2023-08-14 11:08:58 +0300

    harlex

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serhan Bakır</title>
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+
+    <?php
+    include('pages/common/header.php');
+    ?>
+
+    <?php
+    include('pages/common/footer.php');
+    ?>
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
